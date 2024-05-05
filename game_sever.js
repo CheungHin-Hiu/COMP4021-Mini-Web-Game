806429b40860d04f0e72564623c6159a6bb49f74
Andy Hui <chhuiwork@gmail.com>
2024-05-05 17:24:28 +0800

    Set up server for synchronizing player gun image, player score and play time

--- game_sever.js
+++ game_sever.js
@@ -25,27 +25,40 @@ let gameRoom = [];
 //The pending list of the player
 let pendingList = [];
 //Adding a new user to the online user list when the player connects to the WebSocket server
-io.on("connection", (socket) => {
+io.on("connection", 
+(socket) => {
     
     const socketID = socket.id;  //Access the scoket.id value
     if(gameRoom.length ==  0){
-        console.log("pushed first player");
         gameRoom.push(socketID);
     }
     else if(gameRoom.length == 1){
-        console.log("pushed second player");
         gameRoom.push(socketID);
-        socket.broadcast.to(gameRoom[0]).emit("MatchFound", 0);
-        socket.broadcast.to(gameRoom[1]).emit("MatchFound", 1);
+        io.to(gameRoom[0]).emit("MatchFound", 0);
+        io.to(gameRoom[1]).emit("MatchFound", 1);
     }
     else{
         pendingList.push(socketID);
     }
 
-    socket.on("fire hit", (playerID, points) => 
+    //This event tells the opponent that another player have hitted the object i
+    socket.on("fire event", (playerId, awardPoints, objectHitted) => 
     {
-        socket.broadcast.to(gameRoom[(playerID + 1) % 2]).emit("Enemy pint", points);
+        io.to(gameRoom[(playerId + 1) % 2]).emit("enemy hit", awardPoints, objectHitted);
     });
+
+    //This event updates the enemyGun image of the opponent
+    socket.on("graphic info", (playerId, gun_x, gun_y) => {
+        io.to(gameRoom[(playerId + 1) % 2]).emit("graphic update", gun_x, gun_y);
+    });
+
+    //This event synchronize the time of 
+    socket.on("time synchronzation", (timeRemaining) =>
+    {
+        socket.to(gameRoom[1]).emit("time update", timeRemaining);
+    })
+
+    
 
 
 })
